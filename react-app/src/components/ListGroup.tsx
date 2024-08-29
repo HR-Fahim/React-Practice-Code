c61f6e48bc09f9c69509c171b327826f752c24a9
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-08-29 08:31:52 +0600

    refactor: Add conditional rendering for empty list in ListGroup component

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,9 +2,21 @@ import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
   const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+
+  if (items.length === 0) {
+    // if no items
+    return <h1>No Items Found</h1>;
+  }
+
+  // const getMessage = () => {
+  //   return items.length > 3;
+  // };
+
   return (
     <Fragment>
       <h1>List Group</h1>
+      {/* {items.length === 0 ? <h1>No Items Found</h1> : null} */}
+      {/* {getMessage() ? <h1>True</h1> : <h1>False</h1>} */}
       <ul className="list-group">
         {/* <li className="list-group-item">Item 1</li>
         <li className="list-group-item">Item 2</li>

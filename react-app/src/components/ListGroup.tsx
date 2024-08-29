b11e04931c651297394fbeec1b2a3ccfd343f877
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-08-29 06:35:38 +0600

    refactor: Update ListGroup component to use dynamic rendering
    This commit modifies the ListGroup component to use dynamic rendering for the list items. Instead of hardcoding the list items, the component now uses a dynamically generated array of items. This improves flexibility and maintainability of the component.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,19 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
+  const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
   return (
     <Fragment>
       <h1>List Group</h1>
       <ul className="list-group">
-        <li className="list-group-item">Item 1</li>
+        {/* <li className="list-group-item">Item 1</li>
         <li className="list-group-item">Item 2</li>
         <li className="list-group-item">Item 3</li>
         <li className="list-group-item">Item 4</li>
-        <li className="list-group-item">Item 5</li>
+        <li className="list-group-item">Item 5</li> */}
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
       </ul>
     </Fragment>
   );
